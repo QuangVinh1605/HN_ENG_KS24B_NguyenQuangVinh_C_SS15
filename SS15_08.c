90c4f507698a44d6db61ca9e84b75d894d73b344
QuangVinh1605 <nhv16052006@gmail.com>
2024-12-12 23:07:02 +0700

    upload baitap ss15-08

---
+++ SS15_08.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char text[]="nguyen quang vinh";
+	int size=strlen(text);
+	for(int i=0;i<size;i++){
+		if(i==0||text[i-1]==' '){
+			if(isalpha(text[i])){
+				text[i]=toupper(text[i]); 
+			}
+			 
+		} 
+	}
+	printf("%s",text);
+	return 0; 
+	
+}
